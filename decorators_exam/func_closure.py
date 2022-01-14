0dcd0f7aea3cedd394801c4ff484eb2b9a890183
evyakush <evyakush@gmail.com>
2022-01-15 04:54:19 +1000

    Добавили строковую переменную  Добавили функцию минус

--- decorators_exam/func_closure.py
+++ decorators_exam/func_closure.py
@@ -1,6 +1,15 @@
 def plus(a, b):
     return a+b
 
+text ='Работаем'
+
+
+def minus(a,b):
+    return a-b
+
+
 if __name__ == '__main__':
 
     print(plus(2, 3))
+    print(text)
+    print(minus(3, 2))
